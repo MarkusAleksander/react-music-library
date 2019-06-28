2c96cbd1f099e351e3c654739390a2108153dce0
dev <dev@C02NR3MJF8J4.local>
2019-06-28 12:11:27 +0100

    Added click functionality and input component and restyle

--- src/components/Album/Album.js
+++ src/components/Album/Album.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './album.css';
 
 const Album = (props) => {
+
     return (
-        <li className={'album__item album__item--' + (props.owned ? 'owned' : 'required')} onClick={props.click}>{props.artist} - {props.title}</li>
+        <li className={'album__item album__item--' + (props.owned ? 'owned' : 'required')} onClick={props.click}>
+            <p>{props.artist}</p>
+            <p>{props.title}</p>
+        </li>
     );
 }
 
