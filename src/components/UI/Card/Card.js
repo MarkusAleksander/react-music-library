a725391d270914a5f103f8d01d8818d0df6920c8
MarkusAleksander <markholden1989@googlemail.com>
2020-06-16 14:03:56 +0100

    card class name fix

--- src/components/UI/Card/Card.js
+++ src/components/UI/Card/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Card = (props) => (
-    <div className={["card", ...props.className].join(" ")}>
+    <div className={["card", props.className].join(" ")}>
         <div className="card__header">{props.header}</div>
         <div className="card__body">{props.body}</div>
         <div className="card__footer">{props.footer}</div>
