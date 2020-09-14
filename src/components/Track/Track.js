89bdd05754787e4a18771024c03217d8957eed0c
MarkusAleksander <markholden1989@googlemail.com>
2020-09-14 16:44:23 +0100

    tracklist adjustments

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,22 +1,24 @@
 import React, { Component } from "react";
 
 class Track extends Component {
-
     constructor(props) {
         super(props);
 
         this.state = {
-            playing_audio: false
-        }
+            playing_audio: false,
+        };
 
         this.audioRef = React.createRef();
     }
 
     toggleAudio = () => {
-        this.setState({
-            playing_audio: !this.state.playing_audio
-        }, this.handleAudioStatus)
-    }
+        this.setState(
+            {
+                playing_audio: !this.state.playing_audio,
+            },
+            this.handleAudioStatus
+        );
+    };
 
     handleAudioStatus = () => {
         if (this.state.playing_audio) {
@@ -24,31 +26,67 @@ class Track extends Component {
         } else {
             this.audioRef.current.pause();
         }
-    }
+    };
 
     render() {
         return (
             <div className="track" onClick={this.toggleAudio}>
                 <div className="columns is-mobile">
-                    <div className="column is-one-third">
-                        {
-                            this.props.track.album.images && this.props.track.album.images[0] ? <img alt="" src={this.props.track.album.images[0].url} /> : null
-                        }
+                    <div
+                        className="column is-one-third"
+                        style={{ position: "relative" }}
+                    >
+                        {this.props.track.album.images &&
+                        this.props.track.album.images[0] ? (
+                            <img
+                                alt=""
+                                src={this.props.track.album.images[0].url}
+                            />
+                        ) : null}
+                        {this.props.preview_url && !this.state.playing_audio ? (
+                            <i
+                                class="far fa-play-circle"
+                                style={{
+                                    position: "absolute",
+                                    top: "50%",
+                                    left: "50%",
+                                    transform: "translate3d(-50%, -50%, 0)",
+                                    zIndex: "1",
+                                }}
+                            ></i>
+                        ) : this.props.preview_url &&
+                          this.state.playing_audio ? (
+                            <i
+                                class="fas fa-pause"
+                                style={{
+                                    position: "absolute",
+                                    top: "50%",
+                                    left: "50%",
+                                    transform: "translate3d(-50%, -50%, 0)",
+                                    zIndex: "1",
+                                }}
+                            ></i>
+                        ) : null}
                     </div>
                     <div className="column is-two-thirds">
-                        <p className="title is-size-7">{this.props.track.name}</p>
-                        <p className="subtitle is-size-7">{this.props.track.album.name}</p>
+                        <p className="title is-size-7">
+                            {this.props.track.name}
+                        </p>
+                        <p className="subtitle is-size-7">
+                            {this.props.track.album.name}
+                        </p>
                     </div>
-                    <audio
-                        style={{ "display": "none" }}
-                        src={this.props.track.preview_url}
-                        preload={"auto"}
-                        ref={this.audioRef}
-                    >
-                    </audio>
+                    {this.props.track.preview_url ? (
+                        <audio
+                            style={{ display: "none" }}
+                            src={this.props.track.preview_url}
+                            preload={"auto"}
+                            ref={this.audioRef}
+                        ></audio>
+                    ) : null}
                 </div>
             </div>
-        )
+        );
     }
 }
 
